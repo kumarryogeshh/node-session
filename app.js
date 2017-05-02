0dc5f70eb0816732710761c5358bc680467bc1cc
Yogesh Kumar <yogesh.k@signitysolutions.com>
2017-05-02 12:00:23 +0530

    Implementing auth with passport-local and jwt

--- app.js
+++ app.js
@@ -10,11 +10,47 @@ var bodyParser = require('body-parser');
 var mongoose = require('./database/index');
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var stores = require('./routes/store');
 
 var app = express();
 
+//passprt config
+var User = require('./models/user');
+app.use(passport.initialize());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+// function auth (req, res, next) {
+//   console.log(req.headers);
+//
+//   var authHeader = req.headers.authorization;
+//   if(!authHeader) {
+//     var err = new Error('Not Authenticated!');
+//     err.status = 401;
+//     next(err);
+//     return;
+//   }
+//
+//   var auth = new Buffer(authHeader.split(' ')[1],
+//   'base64').toString().split(':');
+//
+//    var user = auth[0];
+//    var pass = auth[1];
+//
+//    //// This is just a dummy example
+//    if(user == 'admin') && pass == 'password'){
+//      next();
+//    } else{
+//      var err = new Error('Not Authenticated!');
+//      err.status = 401;
+//      next(err);
+//    }
+// }
+//
+// app.use(auth);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -35,6 +71,8 @@ app.use('/users', users);
 app.use('/store', stores);
 
 
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -49,7 +87,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.json({
       message: err.message,
       error: err
     });
@@ -60,7 +98,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.json({
     message: err.message,
     error: {}
   });
--- routes/store.js
+++ routes/store.js
@@ -1,16 +1,31 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var Stores = require('../models/store');
-
+var Verify = require('./verify');
 var storeRouter = express.Router();
 storeRouter.use(bodyParser.json());
 
 storeRouter.route('/')
-.get(function(req, res, next){
+.get(Verify.verifyOrdinaryUser, function(req, res, next){
+    console.log(req.decoded._doc.username);
         Stores.find({}, function(err, store){
             if(err) return next(err);
             res.json(store);
         });
     })
 
+  .post(function (req, res, next) {
+        Stores.create(req.body, function(err, store){
+            if(err) return next(err);
+            var id = store.store_id;
+
+            var responseToSend = {
+                "log": "Store added",
+                "data": id,
+                "flag": constants.responseFlags.ACTION_COMPLETE
+            };
+
+            res.status(200).send(responseToSend);
+        });
+      });
 module.exports = storeRouter;
